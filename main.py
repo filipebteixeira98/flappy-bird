c649a329ec2a484d5759bfe3e53a95c6785b6e9c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-03 13:50:57 -0300

    feat: initialize pygame window and basic game loop

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Repository initialized and running successfully!")
+import pygame
 
-# This checks if the file is being run directly
+
+SCREEN_WIDTH = 864
+SCREEN_HEIGHT = 936
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Flappy Bird")
+
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
+    pygame.quit()
+
+
 if __name__ == "__main__":
     main()
