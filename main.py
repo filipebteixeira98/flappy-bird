a3a071a1bdb13451c065014971c6d7b7de148b36
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-03 16:46:15 -0300

    feat: add bird sprite to game scene and create a class that loads the animation

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ SCREEN_HEIGHT = 936
 CLOCK = pygame.time.Clock()
 FPS = 60
 
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        
+        for number in range(1, 4):
+            image = pygame.image.load(f"assets/sprites/bird{number}.png")
+
+            self.images.append(image)
+
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
 
 def main():
     pygame.init()
@@ -15,6 +31,12 @@ def main():
     background_image = pygame.image.load("assets/sprites/bg.png")
     ground_image = pygame.image.load("assets/sprites/ground.png")
 
+    bird_group = pygame.sprite.Group()
+
+    flappy = Bird(100, int(SCREEN_HEIGHT / 2))
+
+    bird_group.add(flappy)
+
     ground_scroll = 0
     ground_speed = 4
 
@@ -24,6 +46,9 @@ def main():
         CLOCK.tick(FPS)
 
         screen.blit(background_image, (0, 0))
+
+        bird_group.draw(screen)
+
         screen.blit(ground_image, (ground_scroll, 768))
 
         ground_scroll -= ground_speed
